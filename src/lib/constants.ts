0233bf017d71e1768f2c56c4cadc14b84c3427a7
TSK_Master <tskmaster.web@gmail.com>
2026-04-13 09:27:09 +0500

    Update default map center

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,8 +1,8 @@
 export const APP_NAME = "GreenStep";
 
 export const DEFAULT_MAP_CENTER = {
-  lat: 52.283306,
-  lng: 76.966694,
+  lat: 52.271145,
+  lng: 76.948489,
 };
 
 export const REPORT_CATEGORIES = [
